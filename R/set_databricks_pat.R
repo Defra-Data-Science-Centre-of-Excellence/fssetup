4ec323c48538b4fe354e03be01b1a411bd6e432a
jpmoatt <joshua.moatt@defra.gov.uk>
2024-10-28 09:26:11 +0000

    update

--- R/set_databricks_pat.R
+++ R/set_databricks_pat.R
@@ -1,24 +1,22 @@
-#' @title 
+#' @title
 #' Set databricks PAT
-#' 
-#' @author Josh Moatt 
-#' 
-#' @description 
+#'
+#' @author Josh Moatt
+#'
+#' @description
 #' Simple function to set databricks PAT. Done via function to avoid committing to GitHub
-#' accidentally. 
-#' 
-#' @return 
+#' accidentally.
+#'
+#' @return
 #' databricks PAT written to environment
-#' 
-#' @export 
+#'
+#' @export
 set_databricks_pat <- function() {
   tmp <- readline("Please enter your databricks PAT: \n")
-  
-  OrganicFarmingStats::print_message(type = "success",
-                                     message = "Databricks PAT set to: {values[1]}",
-                                     values = tmp)
+
+  cli::cli_alert_success("Databricks PAT set to: {tmp}")
   cli::cli_text("")
-  
+
   # return
   tmp
 }
